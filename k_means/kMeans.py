5a091367ba77cf097023e95142e2c2412c7a48b9
darcyfzh <1207217660@qq.com>
2017-04-25 01:50:09 +0800

    add kmeans

---
+++ k_means/kMeans.py
@@ -0,0 +1,133 @@
+'''
+@Author: Darcy
+@Date: April, 24,2017
+@Topic: k means
+'''
+#-*- encoding:utf-8 -*-
+from numpy import * 
+import matplotlib.pyplot as plt
+
+def loadDataSet(fileName):      #general function to parse tab -delimited floats
+    dataMat = []                #assume last column is target value
+    fr = open(fileName)
+    for line in fr.readlines():
+        curLine = line.strip().split('\t')
+        fltLine = map(float,curLine) #map all elements to float()
+        dataMat.append(fltLine)
+    return dataMat
+
+def distEclud(vecA, vecB):
+    return sqrt(sum(power(vecA - vecB, 2))) #la.norm(vecA-vecB)
+
+def randCent(dataSet, k):
+    n = shape(dataSet)[1]
+    centroids = mat(zeros((k,n)))#create centroid mat
+    for j in range(n):#create random cluster centers, within bounds of each dimension
+        minJ = min(dataSet[:,j]) 
+        maxJ = max(dataSet[:,j])
+        rangeJ = float(maxJ - minJ)
+        centroids[:,j] = minJ + rangeJ * random.rand(k,1)
+    return centroids
+    
+def kMeans(dataSet, k, distMeas=distEclud, createCent=randCent):
+    m = shape(dataSet)[0]
+    clusterAssment = mat(zeros((m,2)))#create mat to assign data points 
+                                      #to a centroid, also holds SE of each point
+    centroids = createCent(dataSet, k)
+    clusterChanged = True
+    while clusterChanged:
+        clusterChanged = False
+        for i in range(m):#for each data point assign it to the closest centroid
+            minDist = inf; minIndex = -1
+            for j in range(k):
+                distJI = distMeas(centroids[j,:],dataSet[i,:])
+                if distJI < minDist:
+                    minDist = distJI; minIndex = j
+            if clusterAssment[i,0] != minIndex: 
+            	clusterChanged = True
+            clusterAssment[i,:] = minIndex,minDist ** 2
+        for cent in range(k):#recalculate centroids
+            ptsInClust = dataSet[nonzero(clusterAssment[:,0] == cent)[0]]#get all the point in this cluster
+            centroids[cent,:] = mean(ptsInClust, axis = 0) #assign centroid to mean 
+    return centroids, clusterAssment
+
+def plot(dataSet,centroids):
+	fig = plt.figure()
+	ax = fig.add_subplot(111)
+	ax.scatter(dataSet[:, 0].flatten().A[0], dataSet[:, 1].flatten().A[0])
+	ax.scatter(centroids[:, 0].flatten().A[0], centroids[:, 1].flatten().A[0], marker = '+', s = 300, c = 'red')
+	plt.show()
+
+
+
+'''
+An example  of kMeans in map
+'''
+import urllib
+import json
+def geoGrab(stAddress, city):
+    apiStem = 'http://where.yahooapis.com/geocode?'  #create a dict and constants for the goecoder
+    params = {}
+    params['flags'] = 'J'#JSON return type
+    params['appid'] = 'aaa0VN6k'
+    params['location'] = '%s %s' % (stAddress, city)
+    url_params = urllib.urlencode(params)
+    yahooApi = apiStem + url_params      #print url_params
+    print yahooApi
+    c=urllib.urlopen(yahooApi)
+    return json.loads(c.read())
+
+from time import sleep
+def massPlaceFind(fileName):
+    fw = open('./data/places.txt', 'w')
+    for line in open(fileName).readlines():
+        line = line.strip()
+        lineArr = line.split('\t')
+        retDict = geoGrab(lineArr[1], lineArr[2])
+        if retDict['ResultSet']['Error'] == 0:
+            lat = float(retDict['ResultSet']['Results'][0]['latitude'])
+            lng = float(retDict['ResultSet']['Results'][0]['longitude'])
+            print "%s\t%f\t%f" % (lineArr[0], lat, lng)
+            fw.write('%s\t%f\t%f\n' % (line, lat, lng))
+        else: print "error fetching"
+        sleep(1)
+    fw.close()
+    
+def distSLC(vecA, vecB):#Spherical Law of Cosines
+    a = sin(vecA[0,1]*pi/180) * sin(vecB[0,1]*pi/180)
+    b = cos(vecA[0,1]*pi/180) * cos(vecB[0,1]*pi/180) * \
+                      cos(pi * (vecB[0,0]-vecA[0,0]) /180)
+    return arccos(a + b)*6371.0 #pi is imported with numpy
+
+import matplotlib
+import matplotlib.pyplot as plt
+
+def clusterClubs(numClust=5):
+    datList = []
+    for line in open('./data/places.txt').readlines():
+        lineArr = line.split('\t')
+        datList.append([float(lineArr[4]), float(lineArr[3])])
+    datMat = mat(datList)
+    myCentroids, clustAssing = kMeans(datMat, numClust, distMeas=distSLC)
+    fig = plt.figure()
+    rect=[0.1,0.1,0.8,0.8]
+    scatterMarkers=['s', 'o', '^', '8', 'p', \
+                    'd', 'v', 'h', '>', '<']
+    axprops = dict(xticks=[], yticks=[])
+    ax0=fig.add_axes(rect, label='ax0', **axprops)
+    imgP = plt.imread('./data/Portland.png')
+    ax0.imshow(imgP)
+    ax1=fig.add_axes(rect, label='ax1', frameon=False)
+    for i in range(numClust):
+        ptsInCurrCluster = datMat[nonzero(clustAssing[:,0].A==i)[0],:]
+        markerStyle = scatterMarkers[i % len(scatterMarkers)]
+        ax1.scatter(ptsInCurrCluster[:,0].flatten().A[0], ptsInCurrCluster[:,1].flatten().A[0], marker=markerStyle, s=90)
+    ax1.scatter(myCentroids[:,0].flatten().A[0], myCentroids[:,1].flatten().A[0], marker='+', s=300)
+    plt.show()
+if __name__ == "__main__":
+	dataSet = loadDataSet('./data/testSet.txt')
+	dataMat = mat(dataSet)
+	centroids = kMeans(dataMat, 4)
+	#plot(dataMat, centroids)
+	clusterClubs(5)
+
